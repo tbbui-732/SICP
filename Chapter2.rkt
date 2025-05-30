9f911c7e44bc072bdcd4f160f5eec4e0cb579676
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-05-30 13:18:18 -0400

    Define a procedure to reverse lists

--- Chapter2.rkt
+++ Chapter2.rkt
@@ -1,9 +1,17 @@
 #lang sicp
 
-; exercise 2.17
+; exercise 2.17 - define a procedure that returns the list that contains
+; only the last element of a given non-empty list
 (define (last-pair list)
   (if (eq? (cdr list) nil)
       (car list)
       (last-pair (cdr list))))
 
-; exercise 2.18
+; exercise 2.18 - define a procedure that takes a list as argument and
+; returns a list of the same elements in reverse order
+(define (reverse list)
+  (define (reverse-helper list new-list)
+    (if (eq? (cdr list) nil)
+      (append (cons (car list) '()) new-list)
+      (reverse-helper (cdr list) (append (cons (car list) '()) new-list))))
+  (reverse-helper list '()))
