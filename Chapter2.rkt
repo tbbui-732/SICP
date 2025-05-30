e3484e45b2e12bc0eb98d9057867520770076feb
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-05-30 13:34:56 -0400

    write an optimized tail recursive reverse procedure

--- Chapter2.rkt
+++ Chapter2.rkt
@@ -9,9 +9,18 @@
 
 ; exercise 2.18 - define a procedure that takes a list as argument and
 ; returns a list of the same elements in reverse order
-(define (reverse list)
+; this implementation is O(N^2) due to the append operation
+(define (reverse-naive list)
   (define (reverse-helper list new-list)
-    (if (eq? (cdr list) nil)
+    (if (eq? (cdr list) nil) ; NOTE: eq? is weak since it's not supported in every Scheme version
       (append (cons (car list) '()) new-list)
       (reverse-helper (cdr list) (append (cons (car list) '()) new-list))))
+  (reverse-helper list '()))
+
+; this implementation is O(N)
+(define (reverse list)
+  (define (reverse-helper list new-list)
+    (if (null? list) ; NOTE: null? is a better way to check empty lists
+        new-list
+        (reverse-helper (cdr list) (cons (car list) new-list))))
   (reverse-helper list '()))
